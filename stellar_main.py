ddf4743b3e2d90934f2e245eddebda727e25951c
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2020-04-19 23:54:31 -0700

    4/19

--- stellar_main.py
+++ stellar_main.py
@@ -7,78 +7,81 @@ from flask import jsonify
 app = Flask(__name__)
 CORS(app)
 
-server = Server("https://horizon-testnet.stellar.org")
+class stellarWrapper:
+    def __init__(self, network='test'):
+        if network == 'test':
+            self.server = Server("https://horizon-testnet.stellar.org")
 
-def test():
-	base_fee = server.fetch_base_fee()
-	print(base_fee)
+    @staticmethod
+    def create_account():
+        kp = Keypair.random()
+        print("Public Key is " + kp.public_key)
+        print("Secret Seed is " + kp.secret)
+        return kp
 
-def create_account():
-	kp = Keypair.random()
-	print("Public Key is " + kp.public_key)
-	print("Secret Seed is " + kp.secret)
-	return kp
+    @staticmethod
+    def fund_test_account(publicKey):
+        url = 'https://friendbot.stellar.org'
+        res = requests.get(url, params={'addr': publicKey})
+        print(res)
 
-def fund_test_account(publicKey):
-	url = 'https://friendbot.stellar.org'
-	res = requests.get(url, params={'addr' : publicKey})
-	print(res)
+    def load_account(self, publicKey):
+        account = self.server.load_account(publicKey)
+        print(account)
+        return account
 
-def load_account(publicKey):
-	acc_resp = server.load_account(publicKey)
-	print(acc_resp)
-	return acc_resp
+    def load_server_account(self, publicKey):
+        accountDetails = self.server.accounts().account_id(publicKey).call()
+        print(accountDetails)
+        return accountDetails
 
-def load_server_account(publicKey):
-	acc_resp = server.accounts().account_id(publicKey).call()
-	print(acc_resp)
-	return acc_resp
+    def load_threshold(self, publicKey):
+        accountDetails = self.load_server_account(publicKey)
+        return accountDetails['thresholds']
 
-def load_threshold(publicKey):
-	acc_resp = load_server_account(publicKey)
-	return acc_resp['thresholds']
+    def load_native_balance(self, publicKey):
+        accountDetails = self.load_server_account(publicKey)
+        for x in accountDetails['balances']:
+            if x['asset_type'] == 'native':
+                return x['balance']
 
-def load_native_balance(publicKey):
-	acc_resp = load_server_account(publicKey)
-	for x in acc_resp['balances']:
-		if x['asset_type'] == 'native':
-			return x['balance']
+    def load_sequence(self, publicKey):
+        accountDetails = self.load_server_account(publicKey)
+        return accountDetails['sequence']
 
-def load_sequence(publicKey):
-	acc_resp = load_server_account(publicKey)
-	return acc_resp['sequence']
+    def create_transaction(self, publicKey, operationList):
+        sourceAccount = self.load_account(publicKey)
+        transactionBuilder = TransactionBuilder(
+            source_account=sourceAccount,
+            network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE)
+        transaction = transactionBuilder.build()
+        return transaction.to_xdr()
 
-def create_transaction(publicKey, operationList):
-	sourceAccount = load_account(publicKey)
-	transactionBuilder = TransactionBuilder(
-		source_account=sourceAccount,
-		network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE)
-	transaction = transactionBuilder.build()
-	return transaction.to_xdr()
 
 @app.route('/')
 def main():
-	answer = {}
-	test()
-	kp = create_account()
-	fund_test_account(kp.public_key)
-	load_account(kp.public_key)
-	acc_resp = load_server_account(kp.public_key)
-	thresh = load_threshold(kp.public_key)
-	balance = load_native_balance(kp.public_key)
-	seq = load_sequence(kp.public_key)
-	txn_xdr = create_transaction(kp.public_key, None)
-	answer["Threshold"]=thresh
-	answer["Balance"]=balance
-	answer["Sequence"]= seq
-	answer["Txn XDR"]= txn_xdr
+    answer = {}
+    stellar = stellarWrapper()
+    kp = stellar.create_account()
+    stellar.fund_test_account(kp.public_key)
+    stellar.load_account(kp.public_key)
+    accountDetails = stellar.load_server_account(kp.public_key)
+    thresh = stellar.load_threshold(kp.public_key)
+    balance = stellar.load_native_balance(kp.public_key)
+    seq = stellar.load_sequence(kp.public_key)
+    txn_xdr = stellar.create_transaction(kp.public_key, None)
+    answer["Public_Key"] = kp.public_key
+    answer["Threshold"] = thresh
+    answer["Balance"] = balance
+    answer["Sequence"] = seq
+    answer["Txn XDR"] = txn_xdr
 
-	print("------------Threshold is ", thresh)
-	print("------------Balance is ", balance)
-	print("------------Sequence is ", seq)
-	print("------------Txn xdr is ", txn_xdr)
-	return jsonify(answer), 200
+    print("------------Threshold is ", thresh)
+    print("------------Balance is ", balance)
+    print("------------Sequence is ", seq)
+    print("------------Txn xdr is ", txn_xdr)
+    return jsonify(answer), 200
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='6000')
-
+    app.run(debug=True, host='0.0.0.0', port='5000')
